515f2ab950bcb797722b171896e618f8fe1db503
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-03-19 10:52:25 +0100

    Removed timeline scrolling
    Start over

--- src/index.js
+++ src/index.js
@@ -38,19 +38,3 @@ window.addEventListener("hashchange", router);
 document.addEventListener("events_ready", () => router()); // Wait for the events to be ready before routing
 
 // DEBUG
-
-// select the div with the id "events" with d3
-const events = select("#events");
-// translate to the next event-item with d3 when scrolling
-events.on("wheel", function (e) {
-    const active = document.querySelector("event-item .active");
-    if (!active) return;
-    const delta = e.deltaY;
-    const next = active.parentElement.nextElementSibling;
-    const prev = active.parentElement.previousElementSibling;
-    if (delta > 0 && next) {
-        window.location.hash = `event-${next.getAttribute("year")}`;
-    } else if (delta < 0 && prev) {
-        window.location.hash = `event-${prev.getAttribute("year")}`;
-    }
-});
