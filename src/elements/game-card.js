891003a0b61a2dfdf0b65f2882f4bd641c5d43ad
antoineuldry <antoine.uldry@heig-vd.ch>
2024-04-19 14:48:22 +0200

    Add Platforms to game cards

--- src/elements/game-card.js
+++ src/elements/game-card.js
@@ -4,10 +4,27 @@ class GameCard extends HTMLElement {
         const BASE_URL = '/src/assets/posters/'
         const imgURL = `${BASE_URL}${this.getAttribute('image')}`
         const genres = JSON.parse(this.getAttribute('genres'))
+        const platforms = JSON.parse(this.getAttribute('platforms'))
         const descr = this.getAttribute('descr')
+
+        const TAG_LIMIT = 3
+        const platformsHtmlReady = platforms
+            .splice(0, TAG_LIMIT)
+            .map((p) => `<span>${p}</span>`)
+            .join('')
+        const genresHtmlReady = genres
+            .splice(0, TAG_LIMIT)
+            .map((g) => `<span>${g}</span>`)
+            .join('')
+
         this.innerHTML = `<div class="game-card" style="--background-image: url('${imgURL}')">
-                            <div class="genres">
-                                ${genres.map((g) => `<span>${g}</span>`).join('')}
+                            <div class="tags">
+                                <div class="genres">
+                                    ${genresHtmlReady}
+                                </div>
+                                <div class="platforms">
+                                    ${platformsHtmlReady}
+                                </div>
                             </div>
                             <span class="title">${title}</span>
                             <p>${descr}</p>
